65e4d453c5ada3512a291bed170fa1901088a5b1
Igor Degtyarenko <div50015@mail.ru>
2023-10-03 22:48:13 +0300

    error correction

--- test_hw.py
+++ test_hw.py
@@ -8,7 +8,6 @@ def test_dark_theme_by_time():
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    is_dark_theme = None
     if current_time >= time(22) or current_time <= time(6):
         is_dark_theme = True
     else:
@@ -29,7 +28,6 @@ def test_dark_theme_by_time_and_user_choice():
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    is_dark_theme = None
     if dark_theme_enabled_by_user or current_time >= time(22) or current_time <= time(6):
         is_dark_theme = True
     else:
@@ -84,7 +82,9 @@ def test_find_suitable_user():
 def fun_fn_arg(fn, **kwargs):
     f_name = fn.__name__.replace("_", " ").title()
     args = ", ".join(kwargs.values())
-    return f"{f_name} [{args}]"
+    res = f"{f_name} [{args}]"
+    print(res)
+    return res
 
 
 def test_readable_function():
